e90f9758f0cfe9c5f8edc5bfea226ee50e7a416b
JJ-Amani <gabrieljoshua995@gmail.com>
2026-07-13 23:46:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -142,3 +142,21 @@ themeBtn.onclick=function(){
 document.body.classList.toggle("dark");
 
 }
+
+// ==========================
+// HAMBURGER MENU
+// ==========================
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Close the menu after clicking a link
+document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
